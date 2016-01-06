a623aab25e0012e51ac32f7bbf94637eceb99580
Ilie Ciotir <dipixelhunter@gmail.com>
2016-01-06 11:11:43 +0200

    use the new api for chrome in the notification service

--- src/scripts/stream/notification.service.ts
+++ src/scripts/stream/notification.service.ts
@@ -1,30 +1,39 @@
 import {Injectable} from 'angular2/core';
-declare var Notification: any;
+import Permissions = chrome.permissions;
+import Notifications = chrome.notifications;
 
 @Injectable()
 export class NotificationService {
-	constructor() {
-		if(Notification.permission !== 'granted') {
-			Notification.requestPermission();
-		}
-	}
+    constructor() {
+        Permissions.contains("notifications", (result: boolean) => {
+            if (!result) {
+                Permissions.request("notifications");
+            }
+        });
+    }
 
-	notify(text: string): void {
-		if (!Notification) {
-			alert('Desktop notifications not available in your browser. Try Chromium.');
-		}
+    notify(text: string): void {
+        if (!Notifications) {
+            alert('Desktop notifications not available in your browser. Try Chromium.');
+        }
+        else {
+            Permissions.contains("notifications", (result: boolean) => {
+                if (result) {
+                    this.createNotification(text);
+                } else {
 
-		else if (Notification.permission !== 'granted') {
-			Notification.requestPermission();
-		}
+                    Permissions.request("notifications", (granted: boolean) => {
+                        if (granted) {
+                            this.createNotification(text);
 
-		else {
-			/* tslint:disable:no-unused-expression */
-			new Notification('Real-debrid stream', {
-				icon: './icon.png',
-				body: text
-			});
-			/* tslint:enable:no-unused-expression */
-		}
-	}
+                        }
+                    });
+                }
+            });
+        }
+    }
+    
+    private createNotification(text: string): void {
+        Notifications.create('Real-debrid stream', { contextMessage: text, iconUrl: './icon.png' });
+    }
 }
