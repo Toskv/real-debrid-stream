a31b281f7c4e67d25f66ad08caf20d1399473280
Ilie Ciotir <dipixelhunter@gmail.com>
2016-01-06 18:15:41 +0200

    use the correct way to check and request permissions

--- src/scripts/stream/notification.service.ts
+++ src/scripts/stream/notification.service.ts
@@ -4,24 +4,17 @@ import Notifications = chrome.notifications;
 
 @Injectable()
 export class NotificationService {
-	constructor() {
-		Permissions.contains('notifications', (result: boolean) => {
-			if (!result) {
-				Permissions.request('notifications');
-			}
-		});
-	}
-
 	notify(text: string): void {
+		console.log('notify' + text);
 		if (!Notifications) {
 			alert('Desktop notifications not available in your browser. Try Chromium.');
 		}
 		else {
-			Permissions.contains('notifications', (result: boolean) => {
+			Permissions.contains({ permissions: ['notifications'] }, (result: boolean) => {
 				if (result) {
 					this.createNotification(text);
 				} else {
-					Permissions.request('notifications', (granted: boolean) => {
+					Permissions.request({ permissions: ['notifications'] }, (granted: boolean) => {
 						if (granted) {
 							this.createNotification(text);
 						}
